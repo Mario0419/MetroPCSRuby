87202c226f12efdd6584bf5858f1f48ccb2251c6
Mario Massad <Mario Massad>
2016-04-30 00:15:47 -0400

    Added bootstrap, removed Google Material Design Lite

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
-
+    
   def location
   end
     
   def about
+     
   end
-    def finance
-    end
-    def contactus
-    end
+  def finance
+  end
+  def contactus
+  end
 end
